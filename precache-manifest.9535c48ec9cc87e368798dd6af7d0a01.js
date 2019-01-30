d712e8893acc0c893189915771dcb9c9af26e7e8
181185 <jacobaas@hotmail.com>
2019-01-30 19:16:17 +0100

    Updates

---
+++ precache-manifest.9535c48ec9cc87e368798dd6af7d0a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab2aeb18f9f11ed53830",
+    "url": "/static/js/main.ab2aeb18.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ab2aeb18f9f11ed53830",
+    "url": "/static/css/main.b65cc2d4.chunk.css"
+  },
+  {
+    "revision": "9edf76bddb6783e55f683593fcae2c1c",
+    "url": "/index.html"
+  }
+];
